7985c08f914683d7073f72c931e52e0368e4fdd2
Dominik Gresch <greschd@users.noreply.github.com>
2018-07-05 14:24:19 +0200

    Revert "Add batch submitter"

--- fsc/async_tools/__init__.py
+++ fsc/async_tools/__init__.py
@@ -11,6 +11,5 @@ from ._version import __version__
 
 from ._periodic_task import *
 from ._wrap_to_coroutine import *
-from ._batch_submit import *
 
-__all__ = _periodic_task.__all__ + _wrap_to_coroutine.__all__ + _batch_submit.__all__  # pylint: disable=undefined-variable
+__all__ = _periodic_task.__all__ + _wrap_to_coroutine.__all__  # pylint: disable=undefined-variable
