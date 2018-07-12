0d79c19f96d1ee0e0bc84a5c431715a2c8798598
Dominik Gresch <greschd@gmx.ch>
2018-07-12 15:10:27 +0200

    Use semaphore in limit_parallel.

--- fsc/async_tools/_limit_parallel.py
+++ fsc/async_tools/_limit_parallel.py
@@ -12,7 +12,7 @@ from . import wrap_to_coroutine
 
 
 @export
-def limit_parallel(max_num_parallel, sleep_time=0.):
+def limit_parallel(max_num_parallel):
     """
     Decorator that limits the number of parallel calls to a function or coroutine.
 
@@ -20,25 +20,20 @@ def limit_parallel(max_num_parallel, sleep_time=0.):
     ---------
     max_num_parallel : int
         The maximum number of calls which can run in parallel.
-    sleep_time : float, optional
-        Minimum wait time between checking if the function can be called.
     """
 
     def decorator(func):  # pylint: disable=missing-docstring
-        count = 0
-
+        semaphore = asyncio.Semaphore(value=max_num_parallel)
         func_wrapped = wrap_to_coroutine(func)
 
         @wraps(func)
         async def inner(*args, **kwargs):  # pylint: disable=missing-docstring
-            nonlocal count
-            while count >= max_num_parallel:
-                await asyncio.sleep(sleep_time)
-            count += 1
+            nonlocal semaphore
+            await semaphore.acquire()
             try:
                 res = await func_wrapped(*args, **kwargs)
             finally:
-                count -= 1
+                semaphore.release()
             return res
 
         return inner
